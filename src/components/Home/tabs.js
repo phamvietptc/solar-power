5f89983374d059299f0d27916254a8dd9a97e16d
phamvietptc <phamvietptc@gmail.com>
2023-11-21 17:04:13 +0700

    21/11/2023

---
+++ src/components/Home/tabs.js
@@ -0,0 +1,48 @@
+'use client';
+
+import React, { useState } from 'react';
+
+import { useTabs } from '@/hooks/useTabs';
+import { Framer } from '@/home/lib/framer';
+
+import Industry from './industry';
+import Family from './family';
+import Agriculture from './agriculture';
+
+const Tabs = () => {
+  const [hookProps] = useState({
+    tabs: [
+      {
+        label: 'Công nghiệp',
+        children: <Industry />,
+        id: 'Industry',
+        image: "/images/home/enterprise.png",
+      },
+      {
+        label: 'Hộ gia đình',
+        children: <Family />,
+        id: 'Family',
+        image: "/images/home/family.png",
+      },
+      {
+        label: 'Nông nghiệp',
+        children: <Agriculture />,
+        id: 'Agriculture',
+        image: "/images/home/agriculture.png",
+      },
+    ],
+    initialTabId: 'Matches',
+  });
+  const framer = useTabs(hookProps);
+
+  return (
+    <>
+      <Framer.Tabs {...framer.tabProps} />
+      <div className=" pt-10 w-full text-left float-left before:content-['.'] before:overflow-hidden before:invisible before:w-0 before:h-0 after:clear-both ">
+        {framer.selectedTab.children}
+      </div>
+    </>
+  );
+};
+
+export default Tabs;
